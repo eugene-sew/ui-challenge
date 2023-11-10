8b12301572d51308d4386614413468690d907591
eugene-sew <eugenesew4@gmail.com>
2023-11-10 11:49:38 +0000

    a lot

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import ChallengeDetails from "./ChallengeDetails";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-5 lg:p-24">
       <ChallengeDetails />
     </main>
   );
